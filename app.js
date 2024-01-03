af9a1bbc3f1970892895b35d4e2478d0294c9585
seiya-git <seiya.loveless.mail@gmail.com>
2024-01-03 18:47:32 +0300

    update app

--- app.js
+++ app.js
@@ -3,12 +3,39 @@ import fs from 'fs';
 import { skel2json } from './spine-skel2json.js';
 import { atlas } from './spine-atlas.js';
 
-const filePrefix = './assets/illust_admiral';
-const skelBin  = fs.readFileSync(filePrefix + '.skel');
-const atlasTxt = fs.readFileSync(filePrefix + '.atlas', 'utf8');
+const filePrefix = './assets/';
+const args = process.argv;
 
-console.log('LOG: Input file:', filePrefix);
-const atlasJson = atlas(atlasTxt);
-const skelJson = skel2json(skelBin, atlasJson, 1);
+function help(){
+    console.log('Usage:');
+    console.log(' > node app skel_filename_in_assets_folder');
+    console.log('e.g:');
+    console.log(' > node app admiral');
+    process.exit();
+}
 
-fs.writeFileSync(filePrefix + '_sl2j.json', JSON.stringify(skelJson, null, '    '));
+if(args.length === 3 && args[2].match(/\\/) || args.length === 3 && args[2].match(/\//)){
+    help();
+}
+
+if(args.length !== 3) {
+    help();
+}
+
+try{
+    const fileName = args[2];
+    console.log('LOG: Input file:', filePrefix + fileName);
+    
+    const skelBin  = fs.readFileSync(filePrefix + fileName + '.skel');
+    const atlasTxt = fs.readFileSync(filePrefix + fileName + '.atlas', 'utf8');
+    
+    const atlasJson = atlas(atlasTxt);
+    const skelJson = skel2json(skelBin, atlasJson, 1);
+    
+    fs.writeFileSync(filePrefix + fileName + '_s2j.json', JSON.stringify(skelJson, null, '    '));
+    console.log('LOG: DONE!');
+}
+catch(e){
+    console.log('LOG: Failed!');
+    console.log(e);
+}
