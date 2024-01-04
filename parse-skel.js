1fc0faa8c9787f4e5fcd7656c1f5b299fced4636
seiya-git <seiya.loveless.mail@gmail.com>
2024-01-04 21:25:43 +0300

    rename and move js

--- parse-skel.js
+++ parse-skel.js
@@ -1,7 +1,7 @@
 import fs from 'fs';
 
-import { json2patch } from './spine-skel2json.js';
-import { atlas } from './spine-atlas.js';
+import { skel2json } from './modules/spine-skel2json.js';
+import { atlas } from './modules/spine-atlas.js';
 
 const filePrefix = './assets/';
 const args = process.argv;
@@ -9,7 +9,7 @@ const argc = args.length === 3 ? true : false;
 
 function help(){
     console.log('Usage:');
-    console.log(' > node app json_filename_in_assets_folder');
+    console.log(' > node app skel_filename_in_assets_folder');
     console.log('e.g:');
     console.log(' > node app illust_admiral');
     process.exit();
@@ -28,13 +28,12 @@ try{
     console.log('LOG: Input file:', filePrefix + fileName);
     
     const skelBin  = fs.readFileSync(filePrefix + fileName + '.skel');
-    const skelJson = fs.readFileSync(filePrefix + fileName + '.json', 'utf8');
     const atlasTxt = fs.readFileSync(filePrefix + fileName + '.atlas', 'utf8');
     
     const atlasJson = atlas(atlasTxt);
-    const skelPJson = json2patch(skelBin, skelJson, atlasJson, fileName);
+    const skelJson = skel2json(skelBin, atlasJson, 1);
     
-    fs.writeFileSync(filePrefix + fileName + '_j2p.json', JSON.stringify(skelPJson));
+    fs.writeFileSync(filePrefix + fileName + '_s2j.json', JSON.stringify(skelJson, null, '    '));
     console.log('LOG: DONE!');
 }
 catch(e){
--- patch-json.js
+++ patch-json.js
@@ -1,20 +1,21 @@
 import fs from 'fs';
 
-import { skel2json } from './spine-skel2json.js';
-import { atlas } from './spine-atlas.js';
+import { json2patch } from './modules/spine-skel2json.js';
+import { atlas } from './modules/spine-atlas.js';
 
 const filePrefix = './assets/';
 const args = process.argv;
+const argc = args.length === 3 ? true : false;
 
 function help(){
     console.log('Usage:');
-    console.log(' > node app skel_filename_in_assets_folder');
+    console.log(' > node app json_filename_in_assets_folder');
     console.log('e.g:');
     console.log(' > node app illust_admiral');
     process.exit();
 }
 
-if(args.length === 3 && args[2].match(/\\/) || args.length === 3 && args[2].match(/\//)){
+if(argc && args[2].match(/\\/) || argc && args[2].match(/\//)){
     help();
 }
 
@@ -27,12 +28,13 @@ try{
     console.log('LOG: Input file:', filePrefix + fileName);
     
     const skelBin  = fs.readFileSync(filePrefix + fileName + '.skel');
+    const skelJson = fs.readFileSync(filePrefix + fileName + '.json', 'utf8');
     const atlasTxt = fs.readFileSync(filePrefix + fileName + '.atlas', 'utf8');
     
     const atlasJson = atlas(atlasTxt);
-    const skelJson = skel2json(skelBin, atlasJson, 1);
+    const skelPJson = json2patch(skelBin, skelJson, atlasJson, fileName);
     
-    fs.writeFileSync(filePrefix + fileName + '_s2j.json', JSON.stringify(skelJson, null, '    '));
+    fs.writeFileSync(filePrefix + fileName + '_j2p.json', JSON.stringify(skelPJson));
     console.log('LOG: DONE!');
 }
 catch(e){
