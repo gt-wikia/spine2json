286883884242d6ba13f8096f1ee4448a6254cf51
Suggon <maharjansagun99@gmail.com>
2024-02-01 10:48:11 +0545

    Update skel-parse.bat
    pass 2nd arg for specific model export (`> skel-src.bat visual_novel novel_illust_kaden`)
    .\assets\ -> %_WORK_DIR%
    %_XDIR% toggle for better folder organization

--- skel-src.bat
+++ skel-src.bat
@@ -4,18 +4,37 @@ setlocal
 call batconfigs.bat
 
 set _input=%~n1
-"%_AR_APP%" ".\assets\%_input%" -q > nul
+"%_AR_APP%" "%_WORK_DIR%\%_input%" -q > nul
+if "%~2" NEQ "" set "_input=%~n2" & set "_asset=%~n1" & set "_hasModelName=1"
 
-xcopy "%_AR_SRC%\Assets\" ".\assets\_temp\" /E /C /I /Y /Q > nul
+xcopy "%_AR_SRC%\Assets\" "%_WORK_DIR%\_temp\" /E /C /I /Y /Q > nul
 
-copy ".\assets\_temp\TextAsset\illust_%_input%.skel.bytes"    ".\assets\illust_%_input%.skel"  > nul
-copy ".\assets\_temp\TextAsset\illust_%_input%.atlas.txt"     ".\assets\illust_%_input%.atlas" > nul
-copy ".\assets\_temp\Texture2D\illust_%_input%.rgba4444.png"  ".\assets\illust_%_input%.png"   > nul
-rmdir /S /Q ".\assets\_temp\"
+if "%_XDIR%" NEQ "1" goto extract
 
+mkdir "%_WORK_DIR%\%_input%_temp"
+if "%_hasModelName%" == "1" (
+    copy "%_WORK_DIR%\%_asset%" "%_WORK_DIR%\%_input%_temp" > nul
+) else (
+    move "%_WORK_DIR%\%_input%" "%_WORK_DIR%\%_input%_temp" > nul
+)
+move "%_WORK_DIR%\%_input%_temp" "%_WORK_DIR%\%_input%"     > nul
+move "%_WORK_DIR%\_temp" "%_WORK_DIR%\%_input%\_temp"       > nul
+set _WORK_DIR=%_WORK_DIR%\%_input%
+
+:extract
 set _input_file=illust_%_input%
-set _atlas_illust=./assets/%_input_file%.atlas
+if "%_hasModelName%" == "1" set _input_file=%_input%
 
+copy "%_WORK_DIR%\_temp\TextAsset\%_input_file%.skel.bytes"    "%_WORK_DIR%\%_input_file%.skel"  > nul
+copy "%_WORK_DIR%\_temp\TextAsset\%_input_file%.atlas.txt"     "%_WORK_DIR%\%_input_file%.atlas" > nul
+copy "%_WORK_DIR%\_temp\Texture2D\%_input_file%.rgba4444.png"  "%_WORK_DIR%\%_input_file%.png"   > nul
+copy "%_WORK_DIR%\_temp\Texture2D\%_input_file%.png"           "%_WORK_DIR%\%_input_file%.png"   > nul
+if "%_EXTBG%" == "1" ^
+copy "%_WORK_DIR%\_temp\Texture2D\bg_%_input:illust_=%.png"    "%_WORK_DIR%\"                    > nul  
+rmdir /S /Q "%_WORK_DIR%\_temp\"
+
+set _atlas_illust=%_WORK_DIR%/%_input_file%.atlas
+set _texture_illust=%_WORK_DIR%/%_input_file%.png
 if exist "%_atlas_illust%" goto unpack
 goto end
 
@@ -48,6 +67,8 @@ ffmpeg -hide_banner -loglevel error -i "%_pma_texture%" -vf ^"geq= ^
 :unpack-UPMA
 "%_SPINE%" -u %_SV% -i "%_WORK_DIR%" -o "%_output%" -c "%_atlas_illust%"
 
+if exist "%_pma_atlas%" del "%_atlas_illust:/=\%"
+if exist "%_pma_texture%" del "%_texture_illust:/=\%" & ren "%_pma_texture:/=\%" "%_input_file%.png"
 
 :end
 
